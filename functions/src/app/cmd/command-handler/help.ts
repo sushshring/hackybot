506be56f19c917b737bfb2542832ac13543115fb
Michael Cao <mike.x.cao@gmail.com>
2019-03-16 09:35:57 -0400

    fix hacky replies

--- functions/src/app/cmd/command-handler/help.ts
+++ functions/src/app/cmd/command-handler/help.ts
@@ -1,10 +1,10 @@
-import * as _ from 'lodash';
-import { TopicStore } from '../../db/topic-store';
-import { ICommandResponse } from '../../model/command-response';
-import { ICommandResponder } from '../command-responder';
-import { SlackDmCommandResponder } from '../command-responder/slack-dm-command-responder';
-import { ISubCommand } from '../slack-command/hacky/hacky-command-handler';
-import { ICommandHandler } from './index';
+import * as _ from "lodash";
+import { TopicStore } from "../../db/topic-store";
+import { ICommandResponse } from "../../model/command-response";
+import { ICommandResponder } from "../command-responder";
+import { SlackDmCommandResponder } from "../command-responder/slack-dm-command-responder";
+import { ISubCommand } from "../slack-command/hacky/hacky-command-handler";
+import { ICommandHandler } from "./index";
 
 export class HelpCommand implements ICommandHandler {
   private topicStore: TopicStore;
@@ -18,41 +18,44 @@ export class HelpCommand implements ICommandHandler {
   public async handle(command: ISubCommand): Promise<ICommandResponse> {
     console.log(command);
     // Get the topic for help
-    const topic = _.first(command.subtext!.split(' ')) as string;
+    const topic = _.first(command.subtext!.split(" ")) as string;
     const users = await this.topicStore.getUsersByTopic(topic);
     // Send topic request to mentors
-    await Promise.all(users.map(user => {
-      const dmResponse: ICommandResponse = {
-        text: `You have a new help request for topic: ${topic}.`,
-        users: [user],
-        attachments: [
-          {
-            text: _.tail(command.subtext!.split(' ')).join(' '),
-            callback_id: command.user_id,
-            attachment_type: 'default',
-            color: '#3AA3E3',
-            'actions': [
-              {
-                'name': 'help',
-                'text': 'Let\'s help!',
-                'type': 'button',
-                'value': 'help',
-              },
-              {
-                'name': 'help',
-                'text': 'I can\'t :(',
-                'type': 'button',
-                'value': 'nah',
-              },
-            ],
-          },
-        ],
-      };
-      return this.slackDmResponder.sendResponse(dmResponse);
-    }));
+    await Promise.all(
+      users.map(user => {
+        const dmResponse: ICommandResponse = {
+          text: `You have a new help request for ${topic}`,
+          users: [user],
+          attachments: [
+            {
+              text: _.tail(command.subtext!.split(" ")).join(" "),
+              callback_id: command.user_id,
+              attachment_type: "default",
+              color: "#3AA3E3",
+              actions: [
+                {
+                  name: "help",
+                  text: "Let's help!",
+                  type: "button",
+                  value: "help"
+                },
+                {
+                  name: "help",
+                  text: "I can't :(",
+                  type: "button",
+                  value: "nah"
+                }
+              ]
+            }
+          ]
+        };
+        return this.slackDmResponder.sendResponse(dmResponse);
+      })
+    );
     return {
-      text: 'We\'ve sent your request to mentors. They will DM you if they can help!',
-      responseUrl: command.response_url,
+      text:
+        "We've sent your request to mentors. They will DM you if they can help!",
+      responseUrl: command.response_url
     };
   }
 }
--- functions/src/app/cmd/command-handler/respond-query.ts
+++ functions/src/app/cmd/command-handler/respond-query.ts
@@ -1,11 +1,10 @@
-import { ICommand } from '../../model/command';
-import { ICommandResponse } from '../../model/command-response';
-import { SlackClient } from '../../slack/slack-client';
-import { IRespondCommand } from '../slack-command/hacky/hacky-command-handler';
-import { ICommandHandler } from './index';
+import { ICommand } from "../../model/command";
+import { ICommandResponse } from "../../model/command-response";
+import { SlackClient } from "../../slack/slack-client";
+import { IRespondCommand } from "../slack-command/hacky/hacky-command-handler";
+import { ICommandHandler } from "./index";
 
 export class RespondQueryCommandHandler implements ICommandHandler {
-
   private slackClient: SlackClient;
 
   constructor() {
@@ -14,30 +13,27 @@ export class RespondQueryCommandHandler implements ICommandHandler {
 
   public async handle(command: IRespondCommand): Promise<ICommandResponse> {
     console.log(command);
-    if (command.actions[0].value === 'help') {
-      const result = await this.slackClient.slack.conversations.open({
-        user: command.user!.id,
-        on_behalf_of: command.callback_id,
-      });
-      console.log(result);
+    if (command.actions[0].value === "help") {
       await this.slackClient.slack.chat.postMessage({
-        channel: (result as any).channel.id,
-        text: command.original_message.text,
-        as_user: false,
-      })
+        channel: command.user!.id,
+        text: `${command.original_message.text} from <@${
+          command.callback_id
+        }>. \n\nPlease DM them if possible to help with this topic!`,
+        as_user: true
+      });
     } else {
       await this.slackClient.slack.chat.postMessage({
-        text: 'Okay. Maybe next time :hacky:',
+        text: "Okay. Maybe next time :hacky:",
         as_user: true,
-        channel: command.user.id,
+        channel: command.user.id
       });
     }
     return {
-      text: 'Opening a conversation now...',
+      text: "Opening a conversation now..."
     };
   }
 
   public static parseSubCommand(requestArgs: ICommand): IRespondCommand {
-    return JSON.parse(requestArgs as any as string);
+    return JSON.parse((requestArgs as any) as string);
   }
 }
